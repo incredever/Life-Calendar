08556b23adbdf34dcbd22420e6bb49a88a605172
fantastechk <fantastechk@gmail.com>
2017-05-30 13:23:40 -0700

    init mac app project files

---
+++ Life Calendar/Life Calendar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Life Calendar
+//
+//  Created by Wesley Van der Klomp on 5/30/17.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
