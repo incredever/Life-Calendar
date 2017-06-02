a71acbbdb5f2564dc57e3e0df34104216fb1b47e
fantastechk <fantastechk@gmail.com>
2017-06-02 11:16:29 -0700

    rm representedObject

--- Life Calendar/Life Calendar/ViewController.swift
+++ Life Calendar/Life Calendar/ViewController.swift
@@ -19,13 +19,6 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
+    
 }
 
