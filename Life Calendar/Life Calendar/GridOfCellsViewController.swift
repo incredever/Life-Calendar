1234e5828b43c089ff5ecc656249dfa0947975c0
fantastechk <fantastechk@gmail.com>
2017-06-08 20:56:19 -0700

    add grid of cells window and view controller

---
+++ Life Calendar/Life Calendar/GridOfCellsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GridOfCellsViewController.swift
+//  Life Calendar
+//
+//  Created by Wesley Van der Klomp on 6/8/17.
+//
+
+import Cocoa
+
+class GridOfCellsViewController: NSViewController {
+
+    @IBOutlet weak var sceneView: SCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
