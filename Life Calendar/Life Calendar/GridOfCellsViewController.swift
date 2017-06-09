5e12e4f8b8ae7b11d1214e465150640fe7d0911d
fantastechk <fantastechk@gmail.com>
2017-06-08 21:25:11 -0700

    begin trying to add a cube to the scene

--- Life Calendar/Life Calendar/GridOfCellsViewController.swift
+++ Life Calendar/Life Calendar/GridOfCellsViewController.swift
@@ -15,6 +15,17 @@ class GridOfCellsViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let scene = SCNScene()
+        
+        // http://ronnqvi.st/custom-scenekit-geometry/
+//        
+//        let cellGeometrySource = SCNGeometrySource(normals: [SCNVector3])
+//        let cellGeometry = SCNGeometry(sources: [SCNGeometrySource], elements: nil)
+//        let cell = SCNNode(geometry: <#T##SCNGeometry?#>)
+//        
+//        scene.rootNode.addChildNode(<#T##child: SCNNode##SCNNode#>)
+        
+        
     }
     
 }
