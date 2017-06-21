c1915dc7f5dfaaa51e8f9b0575902ca09291da2e
fantastechk <fantastechk@gmail.com>
2017-06-20 18:28:11 -0700

    add drawing code to Grid

--- macOS Codebase/Grid.swift
+++ macOS Codebase/Grid.swift
@@ -14,12 +14,35 @@ struct Grid {
     
     init() {
         node = SCNNode()
+        
+        drawGridOfCubes(rows: 4, columns: 3)
     }
     
     init(x: CGFloat, y: CGFloat, z: CGFloat) {
         self.init()
         
         node.position = SCNVector3(x: x, y: y, z: z)
+    }
+    
+    private func drawGridOfCubes(rows: Int, columns: Int) {
+        let numberOfRows = rows
+        let numberOfColumns = columns
+        let rowHeight = CGFloat(1)
+        let columnHeight = CGFloat(1)
+        let padding = CGFloat(0.5)
+        
+        for row in 0...numberOfRows {
+            for column in 0...numberOfColumns {
+                let rowNumberAsCGFloat = CGFloat(row)
+                let columnNumberAsCGFloat = CGFloat(column)
+                
+                let x = (rowNumberAsCGFloat * rowHeight) + (rowNumberAsCGFloat * padding)
+                let y = (columnNumberAsCGFloat * columnHeight) + (columnNumberAsCGFloat * padding)
+                let cube = Cube(x: x, y: y, z: 0)
+                
+                node.addChildNode(cube.node)
+            }
+        }
     }
     
 }
