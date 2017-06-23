da3a0ca4389d4fcea4beaecbb32f3c379c2f770e
fantastechk <fantastechk@gmail.com>
2017-06-23 10:26:40 -0700

    Create example vc

---
+++ macOS Codebase/ExampleController.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleController.swift
+//  Life Calendar
+//
+//  Created by Wesley Van der Klomp on 6/23/17.
+//
+
+import Cocoa
+
+class ExampleController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
