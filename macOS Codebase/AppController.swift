e35477f3bfd8d63ebf3c69006e9821e53e8c2760
fantastechk <fantastechk@gmail.com>
2017-06-23 11:46:25 -0700

    create test AppController and attempt to bind

---
+++ macOS Codebase/AppController.swift
@@ -0,0 +1,12 @@
+//
+//  AppController.swift
+//  Life Calendar
+//
+//  Created by Wesley Van der Klomp on 6/23/17.
+//
+
+import Cocoa
+
+class AppController: NSObjectController {
+    let enableSlider = true
+}
