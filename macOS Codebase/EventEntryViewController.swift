3b4f56396b42af451e11ef805d79d8d692b5e985
fantastechk <fantastechk@gmail.com>
2017-06-30 10:55:43 +0300

    Create entry vc file

---
+++ macOS Codebase/EventEntryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EventEntryViewController.swift
+//  Life Calendar
+//
+//  Created by Wesley Van der Klomp on 6/30/17.
+//
+
+import Cocoa
+
+class EventEntryViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
