aefb2bfb83dd99b813d004c19564710481be87ce
fantastechk <fantastechk@gmail.com>
2017-06-30 12:31:38 +0300

    set controller for window in storyboard

---
+++ macOS Codebase/BasicTimelineWindowController.swift
@@ -0,0 +1,18 @@
+//
+//  BasicTimelineWindowController.swift
+//  Life Calendar
+//
+//  Created by Wesley Van der Klomp on 6/30/17.
+//
+
+import Cocoa
+
+class BasicTimelineWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
