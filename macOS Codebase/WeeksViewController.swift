504ec6e2ae99ce7f734d93096e4b1bdcb6970569
fantastechk <fantastechk@gmail.com>
2017-07-07 20:38:29 +0300

    add view outlet

--- macOS Codebase/WeeksViewController.swift
+++ macOS Codebase/WeeksViewController.swift
@@ -6,12 +6,17 @@
 //
 
 import Cocoa
+import SpriteKit
 
 class WeeksViewController: NSViewController {
 
+    @IBOutlet weak var skView: SKView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        
+        
+        
     }
     
 }
