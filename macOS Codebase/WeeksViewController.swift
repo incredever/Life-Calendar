3745764cd494f9a9a2fb3418adc5c2160d66ff6c
fantastechk <fantastechk@gmail.com>
2017-07-07 20:36:59 +0300

    create weeks view controller

---
+++ macOS Codebase/WeeksViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeeksViewController.swift
+//  Life Calendar
+//
+//  Created by Wesley Van der Klomp on 7/7/17.
+//
+
+import Cocoa
+
+class WeeksViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
