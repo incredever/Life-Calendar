0a8d71dfda7f8d2f21b8f6e6c073ec27a6dab4a6
fantastechk <fantastechk@gmail.com>
2017-07-08 12:51:15 +0300

    make addTile take position

--- macOS Codebase/WeeksScene.swift
+++ macOS Codebase/WeeksScene.swift
@@ -13,12 +13,12 @@ class WeeksScene: SKScene {
     override func didMove(to view: SKView) {
         print("did move to view")
         
-        addTiles()
+        addTile(at: CGPoint(x: 0, y: 0))
     }
     
-    func addTiles() {
+    func addTile(at position: CGPoint) {
         let tile = Tile()
-        
+        tile.position = position
         addChild(tile)
     }
 }
