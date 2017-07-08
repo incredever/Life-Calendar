2356574b6e699dad08d9b685c2186978bc1c6c08
fantastechk <fantastechk@gmail.com>
2017-07-08 09:59:59 +0300

    creating from scene class now

--- macOS Codebase/WeeksScene.swift
+++ macOS Codebase/WeeksScene.swift
@@ -12,12 +12,14 @@ class WeeksScene: SKScene {
     
     override func didMove(to view: SKView) {
         print("did move to view")
+        
+        addTiles()
     }
     
     func addTiles() {
         let tileScene = SKScene(fileNamed: "Tile")
         let tileNode = tileScene?.childNode(withName: "Tile")
         
-        
+        addChild(tileNode!)
     }
 }
