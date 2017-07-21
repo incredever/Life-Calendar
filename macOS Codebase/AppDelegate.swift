373cf3f8306295edd4ef615731a7fbcbdc4b8f8e
fantastechk <fantastechk@gmail.com>
2017-07-21 14:59:11 +0300

    add example event

--- macOS Codebase/AppDelegate.swift
+++ macOS Codebase/AppDelegate.swift
@@ -12,7 +12,10 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     func applicationDidFinishLaunching(_ notification: Notification) {
-        let timeline = Timeline(events: [], name: "Testing ABC")
+        
+        let exampleEvent = Event(title: "ABC", colorHex: "000000", starting: Date(), ending: Date())
+        
+        let timeline = Timeline(events: [exampleEvent], name: "Testing ABC")
         
         timeline.saveToDisk()
     }
