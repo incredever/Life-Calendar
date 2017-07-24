04ec79ba0cb24c7f7a9ef083c12bc06d44d25c6b
fantastechk <fantastechk@gmail.com>
2017-07-24 13:54:40 +0300

    call NSApplicationMain

--- macOS Codebase/main.swift
+++ macOS Codebase/main.swift
@@ -10,4 +10,4 @@ import Cocoa
 let delegate = AppDelegate() //alloc main app's delegate class
 NSApplication.shared.delegate = delegate //set as app's delegate
 
-
+NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv);  //start of run loop
