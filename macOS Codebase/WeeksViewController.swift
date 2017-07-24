81f249e8f1a55c0453966adbb49ebbb0d8a94bfc
fantastechk <fantastechk@gmail.com>
2017-07-24 13:30:17 +0300

    make scene match window size

--- macOS Codebase/WeeksViewController.swift
+++ macOS Codebase/WeeksViewController.swift
@@ -16,7 +16,7 @@ class WeeksViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        skScene = WeeksScene(size: CGSize(width: 300, height: 300))
+        skScene = WeeksScene(size: CGSize(width: 600, height: 600))
         skScene?.backgroundColor = NSColor(colorLiteralRed: 246/255, green: 244/255, blue: 241/255, alpha: 1)
         skScene?.scaleMode = .aspectFit
         skView.presentScene(skScene)
