1128320c6f4226da4c8058ddac0aaa9b5899d9a6
fantastechk <fantastechk@gmail.com>
2017-07-25 12:15:44 +0300

    move NSColor extension into it's own file

--- macOS Codebase/Colors.swift
+++ macOS Codebase/Colors.swift
@@ -7,32 +7,6 @@
 
 import Cocoa
 
-// TODO: Rewrite this to be cleaner.
-extension NSColor {
-    convenience init(hexString:String) {
-        
-        let scanner = Scanner(string: hexString)
-        
-        if (hexString.hasPrefix("#")) {
-            scanner.scanLocation = 1
-        }
-        
-        var color:UInt32 = 0
-        scanner.scanHexInt32(&color)
-        
-        let mask = 0x000000FF
-        let r = Int(color >> 16) & mask
-        let g = Int(color >> 8) & mask
-        let b = Int(color) & mask
-        
-        let red   = CGFloat(r) / 255.0
-        let green = CGFloat(g) / 255.0
-        let blue  = CGFloat(b) / 255.0
-        
-        self.init(red:red, green:green, blue:blue, alpha:1)
-    }
-}
-
 enum Colors: String {
     case sceneBackground = "F6F4F1"
 }
---
+++ macOS Codebase/NSColor+init(hexString).swift
@@ -0,0 +1,35 @@
+//
+//  NSColor+init(hexString).swift
+//  Life Calendar
+//
+//  Created by Wesley Van der Klomp on 7/25/17.
+//
+
+import Cocoa
+
+// TODO: Rewrite this to be cleaner.
+extension NSColor {
+    convenience init(hexString:String) {
+        
+        let scanner = Scanner(string: hexString)
+        
+        if (hexString.hasPrefix("#")) {
+            scanner.scanLocation = 1
+        }
+        
+        var color:UInt32 = 0
+        scanner.scanHexInt32(&color)
+        
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        
+        self.init(red:red, green:green, blue:blue, alpha:1)
+    }
+}
+
