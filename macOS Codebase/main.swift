2c5fcf472e82cccd351c1d320ee4d4ac58e092ca
fantastechk <fantastechk@gmail.com>
2017-07-25 14:21:42 +0300

    add basic app menu

--- macOS Codebase/main.swift
+++ macOS Codebase/main.swift
@@ -7,7 +7,48 @@
 
 import Cocoa
 
-let delegate = AppDelegate() // alloc main app's delegate class
-NSApplication.shared.delegate = delegate // set as app's delegate
+let delegate = AppDelegate()
 
-let _ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv); // start of run loop
+func makeMainMenu() -> NSMenu {
+    let mainMenu = NSMenu()
+    let mainAppMenuItem = NSMenuItem(title: "Application", action: nil, keyEquivalent: "")
+    let mainFileMenuItem = NSMenuItem(title: "File", action: nil, keyEquivalent: "")
+    
+    mainMenu.addItem(mainAppMenuItem)
+    mainMenu.addItem(mainFileMenuItem)
+
+    let appMenu = NSMenu()
+    mainAppMenuItem.submenu = appMenu
+
+    let appServicesMenu     = NSMenu()
+    NSApp.servicesMenu      = appServicesMenu
+
+    appMenu.addItem(withTitle: "About Me", action: nil, keyEquivalent: "")
+    appMenu.addItem(NSMenuItem.separator())
+    appMenu.addItem(withTitle: "Preferences...", action: nil, keyEquivalent: ",")
+    appMenu.addItem(NSMenuItem.separator())
+    appMenu.addItem(withTitle: "Hide Me", action: #selector(NSApplication.hide(_:)), keyEquivalent: "h")
+    appMenu.addItem({ () -> NSMenuItem in
+        let m = NSMenuItem(title: "Hide Others", action: #selector(NSApplication.hideOtherApplications(_:)), keyEquivalent: "h")
+        m.keyEquivalentModifierMask = [.command, .option]
+        return m
+        }())
+    appMenu.addItem(withTitle: "Show All", action: #selector(NSApplication.unhideAllApplications(_:)), keyEquivalent: "")
+
+    appMenu.addItem(NSMenuItem.separator())
+    appMenu.addItem(withTitle: "Services", action: nil, keyEquivalent: "").submenu = appServicesMenu
+    appMenu.addItem(NSMenuItem.separator())
+    appMenu.addItem(withTitle: "Quit Me", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
+
+    let fileMenu = NSMenu(title: "File")
+    mainFileMenuItem.submenu = fileMenu
+    fileMenu.addItem(withTitle: "New...", action: #selector(NSDocumentController.newDocument(_:)), keyEquivalent: "n")
+    
+    return mainMenu
+}
+
+NSApplication.shared.mainMenu = makeMainMenu()
+NSApplication.shared.delegate = delegate
+NSApplication.shared.setActivationPolicy(.regular)
+NSApplication.shared.run()
+
