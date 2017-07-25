bde7bb8562ada20a3bed2c0ff271f8a49e50507f
fantastechk <fantastechk@gmail.com>
2017-07-25 11:58:14 +0300

    create colors.swift

---
+++ macOS Codebase/Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Colors.swift
+//  Life Calendar
+//
+//  Created by Wesley Van der Klomp on 7/25/17.
+//
+
+import Cocoa
+
+extension NSColor {
+    static func fromHex(string: String) -> NSColor? {
+        return nil
+    }
+    
+    
+}
+//
+//enum Colors: String {
+//
+//    case sceneBackground: ""
+//}
+
