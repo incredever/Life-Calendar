e1448b12507b90abce4693bbb30ff2d991a1fb10
fantastechk <fantastechk@gmail.com>
2017-07-26 15:46:21 +0300

    add init for Tile2

--- macOS Codebase/Tile2.swift
+++ macOS Codebase/Tile2.swift
@@ -5,4 +5,18 @@
 //  Created by Wesley Van der Klomp on 7/26/17.
 //
 
-import Foundation
+import Cocoa
+import SpriteKit
+
+class Tile2: SKShapeNode {
+    override init() {
+        super.init()
+        
+        self.path = CGPath(rect: CGRect(x: 0, y: 0, width: 10, height: 10), transform: nil)
+        fillColor = NSColor.red
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
