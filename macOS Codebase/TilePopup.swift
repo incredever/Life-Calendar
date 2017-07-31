3e0b601a3a782fc3a9b3bd9b9b39505590ea2cfb
fantastechk <fantastechk@gmail.com>
2017-07-31 16:02:00 +0300

    cleanup

--- macOS Codebase/TilePopup.swift
+++ macOS Codebase/TilePopup.swift
@@ -13,14 +13,11 @@ struct TilePopup {
     let node: SKNode!
     
     init() {
-        
         node = SKNode()
         
         // Create background
         let background = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 20, height: 10))
-        
         background.fillColor = NSColor.lightGray
-        
         node.addChild(background)
     }
     
