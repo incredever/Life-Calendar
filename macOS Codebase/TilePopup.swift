daf00d6856a9c2ac481e9a311a590ecd043051fb
fantastechk <fantastechk@gmail.com>
2017-07-31 18:48:32 +0300

    add label

--- macOS Codebase/TilePopup.swift
+++ macOS Codebase/TilePopup.swift
@@ -19,6 +19,17 @@ struct TilePopup {
         let background = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 20, height: 10))
         background.fillColor = NSColor.lightGray
         node.addChild(background)
+        
+        node.position = CGPoint(x: -10, y: 5)
+        
+        // Create label
+        let label = SKLabelNode(fontNamed: "Avenir Next")
+        label.text = "Hello"
+        label.fontColor = NSColor.red
+        label.fontSize = 14
+        label.position = CGPoint(x: -10, y: 5)
+        
+        node.addChild(label)
     }
     
 }
