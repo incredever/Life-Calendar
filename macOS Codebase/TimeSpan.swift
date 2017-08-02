ba55048e73786d70fc1b0552c3b634e8bd71a2ff
fantastechk <fantastechk@gmail.com>
2017-08-02 11:28:06 +0300

    Add todo

--- macOS Codebase/TimeSpan.swift
+++ macOS Codebase/TimeSpan.swift
@@ -7,6 +7,8 @@
 
 import Cocoa
 
+// TODO: This little structure is a basically the same as DateInterval. So it's probably smart to eventually just use that with extentions as needed.
+
 struct TimeSpan: Codable {
     let start: Date
     let end: Date
