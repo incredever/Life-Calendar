a2d0842ea0d8955f1b1a656964d1da143fd3426e
fantastechk <fantastechk@gmail.com>
2017-08-04 15:48:48 +0300

    add documentation comment to Event

--- macOS Codebase/Event.swift
+++ macOS Codebase/Event.swift
@@ -8,6 +8,7 @@
 import Cocoa
 import SpriteKit
 
+/// The atomic entity of the timeline data structure: representing an event that takes place over a duration of time.
 struct Event: Codable {
     let title: String
     let colorHex: String
