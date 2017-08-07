7310326ab83ea9cbf427951147d112752c33ebe2
fantastechk <fantastechk@gmail.com>
2017-08-07 17:21:28 +0300

    rework YearLabel to contain styling info as properties and generate node when "get"

--- macOS Codebase/YearLabel.swift
+++ macOS Codebase/YearLabel.swift
@@ -11,21 +11,37 @@ import SpriteKit
 /// A label for indicating the year for a particular row of the grid.
 struct YearLabel {
     
-    /// The SpriteKit node for use in the scene.
-    var node: SKLabelNode!
-    
-    /// The primary initializer, containing parameters for all pertinent variables.
-    init(text: String, font: Fonts, size: Int, color: Colors, position: CGPoint) {
-        node = SKLabelNode(fontNamed: font.rawValue)
+    /// A SpriteKit node for use in the scene.
+    var node: SKLabelNode {
+        let node = SKLabelNode(fontNamed: self.fontName.rawValue)
         
-        node.text = text
-        node.fontColor = NSColor(hexString: color.rawValue)
-        node.fontSize = CGFloat(size)
-        node.position = position
+        node.text = self.text
+        node.fontColor = NSColor(hexString: self.fontColor.rawValue)
+        node.fontSize = CGFloat(self.fontSize)
+        node.position = self.position
+        
+        return node
     }
     
+    var text: String
+    var position: CGPoint
+    var fontName: Fonts
+    var fontColor: Colors
+    var fontSize: Int
+    
+    /// The primary initializer, containing parameters for all pertinent variables.
+    private init(text: String, font: Fonts, size: Int, color: Colors, position: CGPoint) {
+
+        self.text = text
+        self.position = position
+        self.fontName = font
+        self.fontColor = color
+        self.fontSize = size
+    }
+
     /// Creates a year label with default styling and position.
     init(text: String) {
+        
         self.init(text: text, font: .yearLabel, size: 14, color: .yearLabelBlack, position: CGPoint(x: 0, y: 0))
     }
     
