76fcb987733a04a311b69370f3c07e63d8246fc2
fantastechk <fantastechk@gmail.com>
2017-08-08 10:31:17 +0300

    Make `tiles` property one dimensional again

--- macOS Codebase/Grid.swift
+++ macOS Codebase/Grid.swift
@@ -30,8 +30,8 @@ struct Grid {
         return n
     }
 
-    /// A two dimensional array of rows of tiles spanning from the `start` date to `end` date. Each tile represents a one week and each row contains 52 tiles, making a full year. The number of rows depends on the number of years are in the lifespan we are working with.
-    var tiles: [[Tile]]
+    /// An array of tiles spanning from the `start` date to `end` date. Each tile represents a one week.
+    var tiles: [Tile]
     
     /// The dimensions at which the grid will be generated.
     let size: CGSize // TODO: initialize as CGSize(width: 330, height: 574)
@@ -55,15 +55,13 @@ struct Grid {
         self.end = end
         self.size = size
         self.position = position
-        
         self.tiles = []
-        // ^ Delete me
         
         let numberOfYears = span.numberOfYearsWithin()
         
         for yearNumber in 0...numberOfYears {
             for weekNumber in 0...52 {
-                // create a Tile with proper span and position
+                // TODO: create a Tile with proper span and position. Then add it to `tiles`.
             }
         }
     }
