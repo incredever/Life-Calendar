8cfe8287d963d10032351ce51b945ae18e24ae8b
fantastechk <fantastechk@gmail.com>
2017-08-21 16:20:55 +0300

    change blankTile color

--- macOS Codebase/Color.swift
+++ macOS Codebase/Color.swift
@@ -10,7 +10,7 @@ import Cocoa
 enum Color: String {
     case sceneBackground = "F6F4F1"
     case yearLabelBlack = "000000"
-    case blankTile = "000001"
+    case blankTile = "BBBBBB"
     case red = "FF0000"
     case green = "00FF00"
     case blue = "0000FF"
