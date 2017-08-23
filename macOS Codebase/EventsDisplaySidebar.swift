3e5a07a342a87684afb661f9dcd45f30074c18a4
fantastechk <fantastechk@gmail.com>
2017-08-23 17:56:31 +0300

    add node to EventsDisplaySidebar

--- macOS Codebase/EventsDisplaySidebar.swift
+++ macOS Codebase/EventsDisplaySidebar.swift
@@ -6,15 +6,22 @@
 //
 
 import Foundation
+import SpriteKit
 
 class EventsDisplaySidebar {
     
     var timeline: Timeline
     
+    var node: SKShapeNode
+    
     private var eventDisplays: [EventDisplay] = []
     
     init(timeline: Timeline) {
         self.timeline = timeline
+        
+        node = SKShapeNode(rectOf: CGSize(width: 100, height: 500))
+        node.fillColor = NSColor.gray
+
     }
     
 }
