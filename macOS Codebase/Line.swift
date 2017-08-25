d8f17a78d4f9818b796cc4e3be0ee233704ea81d
fantastechk <fantastechk@gmail.com>
2017-08-25 08:40:39 +0300

    fill out basics for new Line class

--- macOS Codebase/Line.swift
+++ macOS Codebase/Line.swift
@@ -9,4 +9,24 @@ import Foundation
 
 class Line {
     
+    /// The point at which one end of the line falls within it's parents coordinate system.
+    var start: CGPoint
+    
+    /// The point at which one end of the line falls within it's parents coordinate system.
+    var end: CGPoint
+    
+    /// The stroke width the line should be drawn width.
+    var thickness: Double
+    
+    /// The color the line should be drawn with.
+    var color: Color
+    
+    init(starting: CGPoint, ending: CGPoint) {
+        start = starting
+        end = ending
+        
+        thickness = 2.0
+        color = .black
+    }
+    
 }
