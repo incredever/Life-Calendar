61f9257cf0ef4d18c737a270b431db060ca87fe7
fantastechk <fantastechk@gmail.com>
2017-08-25 11:48:16 +0300

    Add NSBezierPath+cgPath

--- macOS Codebase/Line.swift
+++ macOS Codebase/Line.swift
@@ -33,8 +33,23 @@ class Line {
         thickness = 2.0
         color = .black
         
-        node = SKShapeNode(circleOfRadius: 40)
-        node.fillColor = .blue
+//        node = SKShapeNode(circleOfRadius: 40)
+//        node.fillColor = .blue
+        
+//        var points = [CGPoint(x: 10, y: 10), CGPoint(x: 20, y: 20)]
+//        node = SKShapeNode(points: &points, count: 1)
+//        node.strokeColor = .black
+//        node.lineWidth = 10.0
+        
+        let rect = CGRect(x: 0, y: 0, width: 100, height: 100)
+        var path = CGPath(rect: rect, transform: nil)
+//        let path2 = UIBezierPath()
+//        path2.move(to: starting)
+//        path2.line(to: ending)
+        
+        node = SKShapeNode(path: path)
+        node.strokeColor = .black
+        node.lineWidth = 5.0
     }
     
 }
---
+++ macOS Codebase/NSBezierPath+cgPath.swift
@@ -0,0 +1,62 @@
+//
+//  NSBezierPath+cgPath.swift
+//  Life Calendar
+//
+//  Created by Wesley Van der Klomp on 8/25/17.
+//
+
+import Cocoa
+
+extension NSBezierPath {
+    
+    var cgPath: CGPath {
+        get {
+            return self.transformToCGPath()
+        }
+    }
+    
+    /// Transforms the NSBezierPath into a CGPathRef
+    ///
+    /// :returns: The transformed NSBezierPath
+    private func transformToCGPath() -> CGPath
+    {
+        // Create path
+        let path = CGMutablePath()
+        let points = UnsafeMutablePointer<NSPoint>.allocate(capacity: 3)
+        let numElements = self.elementCount
+        let cgPoint1 = CGPoint(x: points[0].x, y: points[0].y)
+        let cgPoint2 = CGPoint(x: points[1].x, y: points[1].y)
+        let cgPoint3 = CGPoint(x: points[2].x, y: points[2].y)
+        
+        if numElements > 0
+        {
+            var didClosePath = true
+            
+            for index in 0..<numElements
+            {
+                let pathType = self.element(at: index, associatedPoints: points)
+                
+                switch pathType
+                {
+                case .moveToBezierPathElement :
+                    path.move(to: cgPoint1)
+                case .lineToBezierPathElement :
+                    path.addLine(to: cgPoint1)
+                    didClosePath = false
+                case .curveToBezierPathElement :
+                    path.addCurve(to: cgPoint1, control1: cgPoint2, control2: cgPoint3)
+                    didClosePath = false
+                case .closePathBezierPathElement:
+                    path.closeSubpath()
+                    didClosePath = true
+                }
+            }
+            
+            if !didClosePath { path.closeSubpath() }
+        }
+        
+        points.deallocate(capacity: 3)
+        return path
+    }
+    
+}
