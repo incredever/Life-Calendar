8c6a8b562edfb254acfc8a1bb7b87d71edc92a19
fantastechk <fantastechk@gmail.com>
2017-08-28 14:21:02 +0300

    Create EventDisplayNode

---
+++ macOS Codebase/EventDisplayNode.swift
@@ -0,0 +1,51 @@
+//
+//  EventDisplayNode.swift
+//  Life Calendar
+//
+//  Created by Wesley Van der Klomp on 8/28/17.
+//
+
+import Foundation
+import SpriteKit
+
+class EventDisplayNode: SKShapeNode {
+    
+    var owner: EventDisplay?
+    
+    init(position: CGPoint, owner: EventDisplay? = nil) {
+        super.init()
+        
+        self.position = position
+        self.owner = owner
+        self.isUserInteractionEnabled = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+//    override func mouseDown(with event: NSEvent) {
+//        if owner != nil {
+//            owner?.mouseDown(at: event.location(in: self))
+//        }
+//    }
+//
+//    override func mouseDragged(with event: NSEvent) {
+//        if owner != nil {
+//            owner?.mouseDragged(at: event.location(in: self))
+//        }
+//    }
+//
+//    override func mouseUp(with event: NSEvent) {
+//        if owner != nil {
+//            owner?.mouseUp(at: event.location(in: self))
+//        }
+//    }
+//
+//    override func mouseMoved(with event: NSEvent) {
+//        if owner != nil {
+//            owner?.mouseMoved(at: event.location(in: self))
+//        }
+//    }
+    
+}
