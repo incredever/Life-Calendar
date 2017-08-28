5155a61760f044c5abb7e5062ba3f6dfc3143a4d
fantastechk <fantastechk@gmail.com>
2017-08-28 15:11:04 +0300

    set background color on hover

--- macOS Codebase/EventDisplayNode.swift
+++ macOS Codebase/EventDisplayNode.swift
@@ -18,13 +18,14 @@ class EventDisplayNode: SKShapeNode {
         let bottomLeftCorner = CGPoint(x: -(size.width / 2), y: 0)
         let rect = CGRect(origin: bottomLeftCorner, size: size)
         self.path = CGPath(rect: rect, transform: nil)
-        self.fillColor = NSColor(calibratedRed: 1.0, green: 0.0, blue: 0.0, alpha: 0.2)
+        self.fillColor = NSColor(calibratedRed: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
         
         self.position = position
         self.owner = owner
         self.isUserInteractionEnabled = true
     }
     
+ 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -49,6 +50,10 @@ class EventDisplayNode: SKShapeNode {
 //
     override func mouseMoved(with event: NSEvent) {
         print("mouse moved: \(event)")
+        
+        self.fillColor = NSColor(calibratedRed: 1.0, green: 0.0, blue: 0.0, alpha: 0.3)
+
+
 //        if owner != nil {
 //            owner?.mouseMoved(at: event.location(in: self))
 //        }
