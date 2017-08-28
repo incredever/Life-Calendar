bb418871504a5c40b4e61609b197dc6d54b92d4e
fantastechk <fantastechk@gmail.com>
2017-08-28 11:26:22 +0300

    gut WeeksScene and begin moving all that stuff to the VC

--- macOS Codebase/WeeksScene.swift
+++ macOS Codebase/WeeksScene.swift
@@ -15,33 +15,8 @@ class WeeksScene: SKScene {
     var timeline: Timeline?
     
     override func didMove(to view: SKView) {
-        let tempTimeline = Timeline(events: [], name: "temp timeline")
         
-        // Create Grid
-        let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "yyyy-mm-dd"
-        let myBirth = dateFormatter.date(from: "1993-01-05")!
-        let possibleDeath = dateFormatter.date(from: "2083-01-04")!
-        let size = CGSize(width: 330, height: 574)
-        let gridPosition = CGPoint(x: 20, y: Int(size.height) - 10)
-
-        let grid = WeeksGrid(start: myBirth, end: possibleDeath, position: gridPosition)
-
-        grid.delegate = viewController
-        
-        addChild(grid.node)
-        
-        let sidebarSize = CGSize(width: 150, height: frame.height)
-        let sidebarPosition = CGPoint(x: frame.width - sidebarSize.width, y: frame.height)
-        let sidebar = EventsDisplaySidebar(timeline: tempTimeline, size: sidebarSize)
-        sidebar.position = sidebarPosition
-        
-        if let timeline = timeline {
-            sidebar.timeline = timeline
-            grid.timeline = timeline
-        }
-
-        addChild(sidebar.node)
+       
     }
     
 }
