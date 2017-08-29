51ec8aa2ace2c0c5240a57a52647e797cb1bad01
fantastechk <fantastechk@gmail.com>
2017-08-29 22:39:28 +0300

    basic init and draw methods for EventDisplayView

--- macOS Codebase/EventDisplayView.swift
+++ macOS Codebase/EventDisplayView.swift
@@ -9,4 +9,19 @@ import Cocoa
 
 class EventDisplayView: NSView {
     
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+    }
+    
+    required init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ dirtyRect: NSRect) {
+        NSColor.blue.set()
+        
+        let rect = NSRect(x: 0, y: 0, width: 50, height: 50)
+        NSBezierPath(roundedRect: rect, xRadius: 10.0, yRadius: 10.0).fill()
+    }
+    
 }
