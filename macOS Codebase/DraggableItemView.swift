e227bb77240ed43ad8d17d8cbf67b8de6ea9503d
fantastechk <fantastechk@gmail.com>
2017-08-29 08:46:09 +0300

    add basic inits

--- macOS Codebase/DraggableItemView.swift
+++ macOS Codebase/DraggableItemView.swift
@@ -5,8 +5,16 @@
 //  Created by Wesley Van der Klomp on 8/29/17.
 //
 
-import Foundation
+import Cocoa
 
 class DraggableItemView: NSView {
+    
+    override init(frame: NSRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
 }
