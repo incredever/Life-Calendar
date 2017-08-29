e333f67ddd2793d2cb2a1582d73ca4001c2957f2
fantastechk <fantastechk@gmail.com>
2017-08-29 08:25:03 +0300

    add init to SidebarView

--- macOS Codebase/SidebarView.swift
+++ macOS Codebase/SidebarView.swift
@@ -9,6 +9,16 @@ import Cocoa
 
 class SidebarView: NSView {
     
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        
+        print("SidebarView created.")
+    }
+    
+    required init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // tableview
     // table view cells
     
