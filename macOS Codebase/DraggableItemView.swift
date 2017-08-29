9f96962144645be7558aa1bee514f90ccdc51e00
fantastechk <fantastechk@gmail.com>
2017-08-29 09:44:18 +0300

    fill out item properties and basic draw stuff

--- macOS Codebase/DraggableItemView.swift
+++ macOS Codebase/DraggableItemView.swift
@@ -9,12 +9,30 @@ import Cocoa
 
 class DraggableItemView: NSView {
     
+    var itemColor: NSColor!
+    var location: NSPoint!
+    var backgroundColor: NSColor!
+    
     override init(frame: NSRect) {
         super.init(frame: frame)
+        
+        setItemPropertiesToDefault(sender: self)
     }
     
     required init?(coder decoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setItemPropertiesToDefault(sender: AnyObject) {
+        itemColor = NSColor.red
+        location = NSPoint(x: 0, y: 0)
+        backgroundColor = NSColor.white
+    }
+    
+    override func draw(_ dirtyRect: NSRect) {
+        NSColor.white.set()
+        NSBezierPath.fill(dirtyRect)
+        itemColor.set()
     }
     
 }
