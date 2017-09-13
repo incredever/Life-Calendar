13c7a1526a86e9a8a018a2529904f8eca7da8d44
fantastechk <fantastechk@gmail.com>
2017-09-13 19:32:15 +0300

    Remove the title and date drawing from the draw() override because I'll be handling them as subviews

--- macOS Codebase/EventView.swift
+++ macOS Codebase/EventView.swift
@@ -39,20 +39,6 @@ class EventView: NSView {
         NSColor.lightGray.set()
         let topLine = NSBezierPath(rect: NSRect(x: 0, y: dirtyRect.height - 0.5, width: dirtyRect.size.width, height: 0.5))
         topLine.fill()
-
-        let topLeftPoint = CGPoint(x: 5, y: dirtyRect.height)
-        let titlePosition = CGPoint(x: topLeftPoint.x, y: topLeftPoint.y - 23)
-        let title = NSString(string: event.title)
-        title.draw(at: titlePosition, withAttributes: nil)
-
-        let formatter = DateFormatter()
-        formatter.dateFormat = "MMM d, yyyy"
-
-        let startDate = formatter.string(from: event.span.start)
-        let endDate = formatter.string(from: event.span.end)
-
-        let dates = NSString(string: "\(startDate) - \(endDate)")
-        dates.draw(at: NSPoint(x: 5, y: 20), withAttributes: nil)
     }
     
 }
