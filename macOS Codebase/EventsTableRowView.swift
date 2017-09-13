ea104d3e7a3ed6bddf1af3065d56c9b823a37651
fantastechk <fantastechk@gmail.com>
2017-09-13 13:11:05 +0300

    Override drawSelection(in:) with custom background color

--- macOS Codebase/EventsTableRowView.swift
+++ macOS Codebase/EventsTableRowView.swift
@@ -6,10 +6,18 @@ class EventsTableRowView: NSTableRowView {
     private var trackingArea: NSTrackingArea? = nil
     private var mouseIsHovering = false
 
+    override func drawSelection(in dirtyRect: NSRect) {
+        NSColor(calibratedRed: 0.3, green: 0.3, blue: 0.9, alpha: 0.8).set()
+        NSBezierPath(rect: dirtyRect).fill()
+    }
+    
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
 
         if mouseIsHovering {
+            
+//            drawFocusRingMask()
+            
             NSColor(calibratedRed: 0.4, green: 0.4, blue: 1.0, alpha: 1.0).set()
             let path = NSBezierPath(rect: dirtyRect)
             path.lineWidth = 5
