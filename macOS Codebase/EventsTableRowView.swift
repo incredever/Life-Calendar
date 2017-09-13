50d5d4352caebeda3ebeffba57b4f3a2de317e49
fantastechk <fantastechk@gmail.com>
2017-09-13 13:15:53 +0300

    Change selection effect to exactly like the hover effect

--- macOS Codebase/EventsTableRowView.swift
+++ macOS Codebase/EventsTableRowView.swift
@@ -7,17 +7,16 @@ class EventsTableRowView: NSTableRowView {
     private var mouseIsHovering = false
 
     override func drawSelection(in dirtyRect: NSRect) {
-        NSColor(calibratedRed: 0.3, green: 0.3, blue: 0.9, alpha: 0.8).set()
-        NSBezierPath(rect: dirtyRect).fill()
+        NSColor(calibratedRed: 0.4, green: 0.4, blue: 1.0, alpha: 1.0).set()
+        let path = NSBezierPath(rect: dirtyRect)
+        path.lineWidth = 5
+        path.stroke()
     }
     
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
 
         if mouseIsHovering {
-            
-//            drawFocusRingMask()
-            
             NSColor(calibratedRed: 0.4, green: 0.4, blue: 1.0, alpha: 1.0).set()
             let path = NSBezierPath(rect: dirtyRect)
             path.lineWidth = 5
